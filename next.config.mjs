fd6874aede4999ee19ba1747cb6ff67e09726174
Dimas-es <evosdimm@gmail.com>
2025-04-22 18:23:09 +0700

    Updates

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig = {
-  output: 'export',
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio/' : '',
-};
-
-export default nextConfig;
